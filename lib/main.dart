aae74e62d1171d0255803928029e0a72ca49d921
deromon <you@example.com>
2021-07-18 21:48:50 +0530

    day 5 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:flutter_application_1/utility/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -23,10 +24,12 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
+      initialRoute: "/",
       routes: {
+
         "/": (context)=> LoginPage(),
-        "/home": (context)=> HomePage(),
-        "/login": (context)=> LoginPage(),
+        MyRoutes.homeRoutes: (context)=> HomePage(),
+        MyRoutes.loginRoutes: (context)=> LoginPage(),
       },
       
     );
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utility/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({ Key? key }) : super(key: key);
@@ -9,71 +10,77 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-      children: [
-        SizedBox(
-          height: 40,
-
-        ),
-
-        Image.asset(
-          "assets/images/Login_image.png",
-          
-          height:100,
-          width: 100,
-          fit: BoxFit.fitWidth,
-        ),
-        SizedBox(
-          height: 25,
-
-        ),
-        Center(
-        child: Text(
-          "login",
-          style: TextStyle(
-            fontSize: 34,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-
-          ),
-        ),
-      ),
-      SizedBox(
-        height: 20,
-      
-      ),
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 40),
+      child: SingleChildScrollView(
         child: Column(
-                children: [
-                  TextFormField(
-          decoration: InputDecoration(
-            hintText: "Enter UserName",
-            labelText: "Username",
+        children: [
+          SizedBox(
+            height: 40,
+      
+          ),
+      
+          Image.asset(
+            "assets/images/Login_image.png",
+            
+            height:100,
+            width: 100,
+            fit: BoxFit.fitWidth,
+          ),
+          SizedBox(
+            height: 25,
+      
+          ),
+          Center(
+          child: Text(
+            "login",
+            style: TextStyle(
+              fontSize: 34,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+      
+            ),
           ),
         ),
-        TextFormField(
-          obscureText: true,
-          decoration: InputDecoration(
-            hintText: "Enter passwrd",
-            labelText: "Password",
-          ),
+        SizedBox(
+          height: 20,
+        
         ),
-                ],
-              ),
-      ),
-      SizedBox(
-        height: 20,
-      
-      ),
-      ElevatedButton(
-              child: Text("Login"),
-              style: TextButton.styleFrom(),
-              onPressed: () {},
-            )
-      ],
-      
-    )
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 40),
+          child: Column(
+                  children: [
+                    TextFormField(
+            decoration: InputDecoration(
+              hintText: "Enter UserName",
+              labelText: "Username",
+            ),
+          ),
+          TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: "Enter passwrd",
+              labelText: "Password",
+            ),
+          ),
+                  ],
+                ),
+        ),
+        SizedBox(
+          height: 20,
+        
+        ),
+        ElevatedButton(
+                child: Text("Login"),
+                style: TextButton.styleFrom(
+                  minimumSize: Size(100, 50),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, MyRoutes.homeRoutes);
+                },
+              )
+        ],
+        
+          ),
+      )
       
     );
   }
